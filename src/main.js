39f49fead368b68c02cea05d86d62605d8804bba
marksales <marksales.dev.eng@gmail.com>
2022-05-24 23:07:56 -0300

    testcommit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Bootstrap from '/node_modules/bootstrap/dist/css/bootstrap.css'
+
+Vue.use(Bootstrap);
 
 Vue.config.productionTip = false
 
